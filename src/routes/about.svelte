72a3b8797c5cac9115532d3ae6352592b1ed6ddc
fabiorodriguesroque <fabioroque9@gmail.com>
2022-02-21 19:10:00 +0000

    change about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,4 +3,4 @@
 <h1 class="text-2xl text-center my-8 uppercase">About</h1>
 
 
-<p>This is a really cool svelte kit demo that uses the pokemon API to build POKEDEX</p>
+<p>This is a really cool Pokedex built in Svelte!</p>
